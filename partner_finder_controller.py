71228471568f0a3d592bb0e5a2671477d09c9d2a
Regan <rmah@olin.edu>
2020-04-13 21:51:00 -0400

    Starting code

---
+++ partner_finder_controller.py
@@ -0,0 +1,33 @@
+#partner_finder_controller
+class PlayerController:
+    '''
+    A controller that allows the input of the user to change
+    the view on the board.
+
+    Attributes:
+        classroom: model of classroom
+        view:
+    '''
+    def __init__(self):
+        pass
+
+    def move(self, start_spot, end_spot):
+        '''
+        Moves player character depending on the key input
+        given by the player. Movement will be given as such:
+        Move Right = Right arrow
+        Move Left = Left arrow
+        Move Up = Up arrow
+        Move Down = Down arrow
+        Menu = D
+        Select(in menu)/Interact(in classroom) = F
+        Quit Menu = Escape
+        '''
+        pass
+
+    def run(self):
+        '''
+        The main loop to run the game (?)
+        will allow the player to continue moving until the
+        program is quit or a partner is selected.'''
+        pass
