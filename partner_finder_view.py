7b81b18a0d0fee907e287d94f3b4feff2ce82a0d
Regan <rmah@olin.edu>
2020-04-19 15:25:31 -0400

    Added screen initializing/Updated functions

--- partner_finder_view.py
+++ partner_finder_view.py
@@ -1,4 +1,13 @@
 # partner_finder_view
+import Pygame
+
+#Define constants for window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+#Screen object with params SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 class ClassroomView:
     '''
     Class to help visualize the classroom enviornment
