3bbdc1bb8e58d9d3aea9b0ac646371086e11dd87
shadeflower <60320706+shadeflower@users.noreply.github.com>
2020-04-28 21:37:13 -0400

    Moved Window set-up to Run

--- partner_finder_view.py
+++ partner_finder_view.py
@@ -2,13 +2,6 @@
 import Pygame
 import partner_finder_final
 
-#Define constants for window
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-#Screen object with params SCREEN_WIDTH and SCREEN_HEIGHT
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 class classroom:
     '''
     Class to help visualize the classroom enviornment
