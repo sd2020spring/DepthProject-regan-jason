3cde8ba5b4cae6100388bc0637e57585e1969cea
Regan <rmah@olin.edu>
2020-05-01 18:28:59 -0400

    Edit Draw function

--- partner_finder_controller.py
+++ partner_finder_controller.py
@@ -1,6 +1,5 @@
 #partner_finder_controller
 import pygame
-
 from pygame.locals import(
     K_UP,
     K_DOWN,
@@ -12,25 +11,27 @@ from pygame.locals import(
 )
 
 
+blue = (0,0,225)
+
 class Player(pygame.sprite.Sprite):
     '''
     This will be the sprite/model for the current player
     '''
-    def __init__(self):
+    def __init__(self, x, y):
 
         distance = 2
 
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface ((2,2)) #We'll decide on sizing
-        self.image.fill() # Character is a black square sized 2,2 for now
-        self.rect = self.image.get_rect(WHITE)
+        self.image.fill(blue) # Character is a black square sized 2,2 for now
+        self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.rect.center = (distance / 2, distance / 2)
 
     def move(self, speedx, speedy):
         self.rect.x += speedx
         self.rect.y += speedy
 
     def draw(self):
-        window.blit(self.image.get_rect(self.rect.x, self.rect.y))
+        screen.blit(self.image.get_rect(self.rect.x, self.rect.y)
