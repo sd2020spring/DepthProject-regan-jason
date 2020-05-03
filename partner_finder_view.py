e72a932c3ae1a345f9c671c29ba0165130af8a74
Lin <jlin8@babson.edu>
2020-05-03 14:23:44 -0400

    added map creation

--- partner_finder_view.py
+++ partner_finder_view.py
@@ -1,35 +1,60 @@
 # partner_finder_view
 import pygame
-import partner_finder_final
+from partner_finder_final import struc_Tile, com_Classmate
+from constants import *
+from partner_finder_controller import *
+from partner_finder_run import *
+import partner_finder_run
 
 
+'''
+.___  ___.      ___      .______
+|   \/   |     /   \     |   _  \
+|  \  /  |    /  ^  \    |  |_)  |
+|  |\/|  |   /  /_\  \   |   ___/
+|  |  |  |  /  _____  \  |  |
+|__|  |__| /__/     \__\ | _|      '''
 
-class Classroom:
+
+''' the 0 to 30 represents that there are 30 tiles'''
+
+def map_create():
     '''
     Class to help visualize the classroom enviornment
 
     Attributes: a Classroom instance representing the room to EnvironmentView
     '''
-    def __init__(self, HEIGHT, WIDTH):
-        self.HEIGHT = HEIGHT
-        self.WIDTH = WIDTH
+    new_map = [[ struc_Tile(False) for y in range(0, CELL_HEIGHT)] for x in range(0, CELL_WIDTH) ]
+
+    new_map[10][10].block_path = True
+    new_map[10][15].block_path = True
+
+    return new_map
 
 
-    def draw(self):
-        '''
-        Draws the layout of the classroom.
-        with tables/people
-        Returns: The current layout of the Classroom
-        '''
-        screen.blit(self.image.get_rect(self.HEIGHT, self.WIDTH))
+'''''''''
+  ____
+ |  _ \ _ __ __ ___      _(_)_ __   __ _
+ | | | | '__/ _` \ \ /\ / / | '_ \ / _` |
+ | |_| | | | (_| |\ V  V /| | | | | (_| |
+ |____/|_|  \__,_| \_/\_/ |_|_| |_|\__, |
+                                  |___/        '''''''''
 
-    def add_students(self):
-        '''
-        Function will add x amount of students to populate the
-        classroom. Each student will have their own spawn point.
-        Treats students in an kind of x,y coordinate system with
-        students occupying squares.
-        '''
-        for object in students:
-            self.x = random.choice(xposition)
-            self.y = random.choice(yposition)
+
+
+
+
+
+
+
+
+def add_students(self):
+    '''
+    Function will add x amount of students to populate the
+    classroom. Each student will have their own spawn point.
+    Treats students in an kind of x,y coordinate system with
+    students occupying squares.
+    '''
+    for object in students:
+        self.x = random.choice(xposition)
+        self.y = random.choice(yposition)
