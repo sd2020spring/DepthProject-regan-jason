50972811ea57c03112bcbb661c3b593c8340b41d
Regan <rmah@olin.edu>
2020-05-07 19:43:41 -0400

    changes

--- partner_finder_view.py
+++ partner_finder_view.py
@@ -33,35 +33,5 @@ def map_create():
     for i in range(25):
         new_map[i][18].block_path = True
 
-
-
-
+    #ne
     return new_map
-
-
-'''''''''
-  ____
- |  _ \ _ __ __ ___      _(_)_ __   __ _
- | | | | '__/ _` \ \ /\ / / | '_ \ / _` |
- | |_| | | | (_| |\ V  V /| | | | | (_| |
- |____/|_|  \__,_| \_/\_/ |_|_| |_|\__, |
-                                  |___/        '''''''''
-
-
-
-
-
-
-
-
-#
-# def add_students(self):
-#     '''
-#     Function will add x amount of students to populate the
-#     classroom. Each student will have their own spawn point.
-#     Treats students in an kind of x,y coordinate system with
-#     students occupying squares.
-#     '''
-#     for object in students:
-#         self.x = random.choice(xposition)
-#         self.y = random.choice(yposition)
